b20c51931902e6492fe248abd990720dffc1eb23
Diego Giuliani <giuliani.diego@gmail.com>
2021-02-05 08:32:05 -0300

    Setup initial layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,40 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+  const logout = () => {};
+  return (
+    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-800 h-16">
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img src="/home-color.svg" alt="Home" className="inline w-6" />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={() => logout()}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a>Login / Signup</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main style={{ minHeight: "calc(100vh - 70px)" }}>{main}</main>
+    </div>
+  );
+};
+
+export default Layout;
